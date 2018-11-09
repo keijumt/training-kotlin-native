1fcef8a05f68bccccb0ef06a1bc5f6ad13246d24
keijumt <keijumt@gmail.com>
2018-11-09 22:13:33 +0900

    Add project

---
+++ app/src/main/java/keijumt/training_kotlin_native/MainActivity.kt
@@ -0,0 +1,12 @@
+package keijumt.training_kotlin_native
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
