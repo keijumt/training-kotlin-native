30216284c2a38281cde3e46ee15e4dcc96e6af02
keijumt <keijumt@gmail.com>
2018-11-11 23:40:07 +0900

    Show user name on android

--- android/app/src/main/java/keijumt/training_kotlin_native/MainActivity.kt
+++ android/app/src/main/java/keijumt/training_kotlin_native/MainActivity.kt
@@ -1,15 +1,29 @@
 package keijumt.training_kotlin_native
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import keijumt.createApplicationScreenMessage
+import keijumt.api.GithubApi
+import keijumt.model.User
+import keijumt.presentation.presenter.GithubPresenterImpl
+import keijumt.presentation.view.GithubView
+import keijumt.repository.GithubRepositoryImpl
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GithubView {
+
+    private val presenter by lazy { GithubPresenterImpl(Dispatchers.Main, this, GithubRepositoryImpl(GithubApi())) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        text_sample.text = createApplicationScreenMessage()
+
+        presenter.fetchUser("keijumt")
+    }
+
+    override fun showUser(user: User) {
+        progress_bar.visibility = View.GONE
+        text_sample.text = user.name
     }
 }
